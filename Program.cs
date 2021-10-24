9cff197459805289caba8ff7553d21ad48fa36a0
Fbaldesamo <baldesamofranco@gmail.com>
2021-10-24 22:09:25 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Mp1_Baldesamo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            AddUser();
+
+            Console.ReadKey();
+        }
+
+        public static void AddUser()
+        {
+            bool validate;
+            string Firstname;
+            string Lastname;
+            do
+            {
+                Console.Write("First Name: ");
+                Firstname = Console.ReadLine();
+                validate = ValidateUser(Firstname);
+
+                
+            } while (validate == false);
+
+            do
+            {
+                Console.Write("Last Name: ");
+                Lastname = Console.ReadLine();
+                validate = ValidateUser(Lastname);
+
+            }
+            while (validate == false);
+            Console.WriteLine("Correct!");
+
+            
+        }
+
+        public static bool ValidateUser(string a)
+        {
+            if (string.IsNullOrEmpty(a))
+            {
+                return false;
+            }
+
+            foreach (char c in a)
+            {
+                while (char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
